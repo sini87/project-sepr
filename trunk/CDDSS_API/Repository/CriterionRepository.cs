d236f6a27dcd91da63bd6ed40ec1387ebf9ac762
andreas.knogler87@gmail.com <andreas.knogler87@gmail.com@70bb1a0d-f189-3689-9456-296aa23f10ae>
2014-11-26 15:01:51 +0000

    Implementierung von CriterionController und CriterionRepository

--- trunk/CDDSS_API/Repository/CriterionRepository.cs
+++ trunk/CDDSS_API/Repository/CriterionRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CDDSS_API.Models.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,5 +9,69 @@ namespace CDDSS_API.Repository
 
     public class CriterionRepository : RepositoryBase
     {
-    }
+        /// <summary>
+        /// returns a Criterion by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public Criterion GetCriterion(int id)
+        {
+            Criterion criterion = new Criterion();;
+            var query = from Criterion in ctx.Criterion
+                        where
+                          Criterion.Id == id
+                        select new
+                        {
+                            Id = Criterion.Id,
+                            Name = Criterion.Name,
+                            Description = Criterion.Description,
+                            Issue = Criterion.Issue,
+                            Weight = Criterion.Weight
+                        };
+            foreach (var c in query)
+            {
+                criterion.Id = c.Id;
+                criterion.Issue = c.Issue;
+                criterion.Name = c.Name;
+                criterion.Weight = c.Weight;
+                criterion.Description = c.Description;
+            }
+            return criterion;
+        }//EndGetCriterion(id)
+
+        /// <summary>
+        /// Return all Criterias by IssueID
+        /// </summary>
+        /// <param name="issueID"></param>
+        /// <returns></returns>
+        public List<Criterion> GetCriteriasByIssue(int issueID)
+        {
+            List<Criterion> allIssueCriterias = new List<Criterion>();
+            var query = from Criterion in ctx.Criterion
+                        where
+                          Criterion.Issue == issueID
+                        select new
+                        {
+                            Id = Criterion.Id,
+                            Name = Criterion.Name,
+                            Description = Criterion.Description,
+                            Issue = Criterion.Issue,
+                            Weight = Criterion.Weight
+                        };
+            foreach (var c in query)
+            {
+                Criterion criterion = new Criterion();
+                criterion.Id = c.Id;
+                criterion.Issue = c.Issue;
+                criterion.Name = c.Name;
+                criterion.Weight = c.Weight;
+                criterion.Description = c.Description;
+                allIssueCriterias.Add(criterion);
+            }
+            return allIssueCriterias;
+        }
+
+
+
+    }//EndClass
 }
