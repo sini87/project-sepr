f07389deedff36392418d69b22404e622988f9c2
sinisa.zubic <sinisa.zubic@gmx.at>
2014-11-26 11:24:28 +0000

    bugfixing

--- Client/Default.aspx.designer.cs
+++ Client/Default.aspx.designer.cs
@@ -29,5 +29,7 @@ namespace Client {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ListBox ListBox;
+
+        ///CHECKOUT
     }
 }
