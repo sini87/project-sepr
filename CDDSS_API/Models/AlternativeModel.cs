bfbd4d5089d5406c3ec765553c0912958378ed36
sinisa.zubic <sinisa.zubic@gmx.at>
2014-11-30 01:52:58 +0000

    Alternative Controller - GET, Create, Update, Delete

--- CDDSS_API/Models/AlternativeModel.cs
+++ CDDSS_API/Models/AlternativeModel.cs
@@ -1,11 +1,75 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Web;
 
 namespace CDDSS_API.Models
 {
+    [DataContract]
     public class AlternativeModel
     {
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        public int Issue { get; set; }
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public string Description { get; set; }
+        [DataMember]
+        public string Reason { get; set; }
+        [DataMember]
+        public double Rating { get; set; }
+
+        public AlternativeModel()
+        {
+            Id = -1;
+            Issue = -1;
+            Name = "";
+            Description = "";
+            Reason = "";
+            Rating = -1.0;
+        }
+
+        public AlternativeModel(int issue, string name)
+        {
+            Id = -1;
+            Issue = issue;
+            Name = name;
+            Description = "";
+            Reason = "";
+            Rating = -1;
+        }
+
+        public AlternativeModel(int issue, string name, string description, string reason)
+        {
+            Issue = issue;
+            Id = -1;
+            Name = name;
+            Description = description;
+            Reason = reason;
+            Rating = -1;
+        }
+
+        public AlternativeModel(int id, int issue, string name, string description, string reason)
+        {
+            Id = id;
+            Issue = issue;
+            Name = name;
+            Description = description;
+            Reason = reason;
+            Rating = -1;
+        }
+
+        public AlternativeModel(int id, int issue, string name, string description, string reason, double rating)
+        {
+            Issue = issue;
+            Id = id;
+            Name = name;
+            Description = description;
+            Reason = reason;
+            Rating = rating;
+        }
     }
 }
--- CDDSS_API/Repository/AlternativeRepository.cs
+++ CDDSS_API/Repository/AlternativeRepository.cs
@@ -1,11 +1,123 @@
-﻿using System;
+﻿using CDDSS_API.Models;
+using CDDSS_API.Models.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace CDDSS_API.Repository
 {
-    public class AlternativeRepository
+    public class AlternativeRepository : RepositoryBase
     {
+        public List<AlternativeModel> GetAlternativesByIssueId(int issueId)
+        {
+            List<AlternativeModel> list = new List<AlternativeModel>();
+            AlternativeModel altModel;
+
+            foreach (Alternative alt in ctx.Alternatives.Where(x => x.Issue == issueId).ToList())
+            {
+                altModel = new AlternativeModel();
+                altModel.Id = alt.Id;
+                altModel.Name = alt.Name;
+                altModel.Issue = alt.Issue;
+
+                if (alt.Description != null)
+                {
+                    altModel.Description = alt.Description;
+                }
+                if (alt.Reason != null)
+                {
+                    altModel.Reason = alt.Reason;
+                }
+                if (alt.Rating != null)
+                {
+                    altModel.Rating = (double)alt.Rating;
+                }
+                list.Add(altModel);
+            }
+
+            return list;
+        }
+
+        public bool CreateAlternativeForIssue(AlternativeModel altModel)
+        {
+            Alternative alt = new Alternative();
+
+            try
+            {
+                var query = from Alternatives in
+                                (from Alternatives in ctx.Alternatives
+                                 select new
+                                 {
+                                     Alternatives.Id,
+                                     Dummy = "x"
+                                 })
+                            group Alternatives by new { Alternatives.Dummy } into g
+                            select new
+                            {
+                                MaxId = (int)g.Max(p => p.Id)
+                            };
+
+                int altId;
+                if (query.ToList().Count == 0)
+                {
+                    altId = 1;
+                }
+                else
+                {
+                    altId = query.ToList()[0].MaxId + 1;
+                }
+
+                alt.Id = altId;
+                alt.Name = altModel.Name;
+                alt.Issue = altModel.Issue;
+                alt.Description = altModel.Description;
+                alt.Reason = altModel.Reason;
+
+                ctx.Alternatives.InsertOnSubmit(alt);
+                ctx.SubmitChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return false;
+        }
+
+        public bool UpdateAlternative(AlternativeModel altModel)
+        {
+            if (ctx.Alternatives.Where(x => x.Id == altModel.Id).ToList().Count <= 0){
+                return false;
+            }else{
+                Alternative alt = ctx.Alternatives.Where(x => x.Id == altModel.Id).First();
+                alt.Name = altModel.Name;
+                alt.Reason = altModel.Reason;
+                alt.Description = altModel.Description;
+
+                ctx.SubmitChanges();
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool DeleteAlternative(int altId)
+        {
+            if (ctx.Alternatives.Where(x => x.Id == altId).ToList().Count <= 0)
+            {
+                return false;
+            }
+            else
+            {
+                Alternative alt = ctx.Alternatives.Where(x => x.Id == altId).First();
+                ctx.Alternatives.DeleteOnSubmit(alt);
+                ctx.SubmitChanges();
+                return true;
+            }
+
+            return false;
+        }
     }
 }
