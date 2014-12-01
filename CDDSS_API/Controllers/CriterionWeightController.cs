8035ce8078858b4578376734a20cfd3227d71ebb
sinisa.zubic <sinisa.zubic@gmx.at>
2014-12-01 19:17:18 +0000

    CriterionWeight - Add, Update, Insert, Delete Already Tested

---
+++ CDDSS_API/Controllers/CriterionWeightController.cs
@@ -0,0 +1,91 @@
+﻿using CDDSS_API.Models;
+using CDDSS_API.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace CDDSS_API.Controllers
+{
+    [EnableCors("http://localhost:51853", "*", "*")]
+    [Authorize]
+    public class CriterionWeightController : ApiController
+    {
+        CriterionWeightRepository cwRepo;
+
+        public CriterionWeightController()
+        {
+            cwRepo = new CriterionWeightRepository();
+        }
+
+        /// <summary>
+        /// Adds criterion weights to a group of criteria of the logged in user
+        /// </summary>
+        /// <param name="list">group of criterion weights, user and accessobject must not be set</param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("api/CriterionWeight/Add")]
+        public HttpResponseMessage AddCriterionWeights(List<CriterionWeightModel> list)
+        {
+            if (cwRepo.AddCriterionWeights(list, RequestContext.Principal.Identity.Name))
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, "OK");
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.NotAcceptable, "Something went wrong");
+            }
+        }
+
+        /// <summary>
+        /// Returns all criterion weights of an issue
+        /// </summary>
+        /// <param name="issueId"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public List<CriterionWeightModel> GetCriterionWeightsForIssue(int issueId)
+        {
+            return cwRepo.GetCriterionWeights(issueId);
+        }
+
+        /// <summary>
+        /// updates criterion wieghts of the current logged user
+        /// </summary>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("api/CriterionWeight/Update")]
+        public HttpResponseMessage EditCriterionWeights(List<CriterionWeightModel> list)
+        {
+            if (cwRepo.UpdateCriterionWeights(list, RequestContext.Principal.Identity.Name))
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, "OK");
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.NotAcceptable, "Something went wrong");
+            }
+        }
+
+        /// <summary>
+        /// deletes criterion weights for issue of logged in user
+        /// </summary>
+        /// <param name="issueId"></param>
+        /// <returns></returns>
+        [HttpDelete]
+        public HttpResponseMessage DeleteCriterionWeights(int issueId)
+        {
+            if (cwRepo.DeleteCriterionWeights(issueId, RequestContext.Principal.Identity.Name))
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, "OK");
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.NotAcceptable, "Something went wrong");
+            }
+        }
+    }
+}
---
+++ CDDSS_API/Models/CriterionWeightModel.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace CDDSS_API.Models
+{
+    [DataContractAttribute]
+    public class CriterionWeightModel
+    {
+        /// <summary>
+        /// Access Object ID of User
+        /// </summary>
+        [DataMember]
+        public int UserAccesObject { get; set; }
+        [DataMember]
+        public string UserId { get; set; }
+        [DataMember]
+        public int Criterion { get; set; }
+        [DataMember]
+        public double Weight { get; set; }
+
+        public CriterionWeightModel()
+        {
+            UserId = "";
+            UserAccesObject = -1;
+            Criterion = -1;
+            Weight = -1;
+        }
+
+        /// <summary>
+        /// Creates a new CriterionWeightModel
+        /// </summary>
+        /// <param name="userId">Access Object ID of User</param>
+        /// <param name="criterion">Criterion ID</param>
+        /// <param name="weight"></param>
+        public CriterionWeightModel(int userAccesObject, int criterion, double weight)
+        {
+            UserId = "";
+            UserAccesObject = userAccesObject;
+            Criterion = criterion;
+            Weight = weight;
+        }
+
+        /// <summary>
+        /// Creates a new CriterionWeightModel
+        /// </summary>
+        /// <param name="user">Access Object ID of User</param>
+        /// <param name="criterion">Criterion ID</param>
+        /// <param name="weight"></param>
+        public CriterionWeightModel(string userId, int criterion, double weight)
+        {
+            UserAccesObject = -1;
+            UserId = userId;
+            Criterion = criterion;
+            Weight = weight;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="criterion">criterion id</param>
+        /// <param name="weight"></param>
+        public CriterionWeightModel(int criterion, double weight)
+        {
+            Criterion = criterion;
+            Weight = weight;
+        }
+
+        public CriterionWeightModel(int userAccesObject, string userId, int criterion, double weight)
+        {
+            UserAccesObject = userAccesObject;
+            UserId = userId;
+            Criterion = criterion;
+            Weight = weight;
+        }
+    }
+}
