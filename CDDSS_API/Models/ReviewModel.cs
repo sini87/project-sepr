f5e436843c4ee6802dde9ea548e68f3a69f392a6
sinisa.zubic <sinisa.zubic@gmx.at>
2014-12-02 19:44:46 +0000

    ReviewController - Add, Edit, Delete, Get

---
+++ CDDSS_API/Models/ReviewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace CDDSS_API.Models
+{
+    [DataContract]
+    public class ReviewModel
+    {
+        [DataMember]
+        public int Issue { get; set; }
+        [DataMember]
+        public int Rating { get; set; }
+        [DataMember]
+        public string Explanation { get; set; }
+        [DataMember]
+        public string UserFirstName { get; set; }
+        [DataMember]
+        public string UserLastName { get; set; }
+
+        public ReviewModel()
+        {
+            UserFirstName = "";
+            UserLastName = "";
+            Rating = -1;
+            Explanation = "";
+            Issue = -1;
+        }
+        
+        public ReviewModel(int issueId, int rating, string explanation)
+        {
+            Issue = issueId;
+            UserFirstName = "";
+            UserLastName = "";
+            Rating = rating;
+            Explanation = explanation;
+        }
+    }
+}
