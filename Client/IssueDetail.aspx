e254da6a76ec2bbd9eafb537cd18323d34b760c1
daniel.kaltenbrunner <daniel.kaltenbrunner@gmail.com>
2014-12-04 18:25:32 +0000

    Added IssueDetail.aspx TODO: Add criteria, comment and alternatives

---
+++ Client/IssueDetail.aspx
@@ -0,0 +1,60 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="IssueDetail.aspx.cs" Inherits="Client.IssueDetail" %>
+
+<asp:Content runat="server" ID="BodyContent" ContentPlaceHolderID="MainContent">
+    <h1>Issue</h1>
+    <br />
+    <hr />
+    <asp:Panel runat="server" ID="issueTitle"></asp:Panel><br />
+
+    <asp:Panel runat="server" ID="rating" /><br />
+    <asp:Panel runat="server" ID="tag"></asp:Panel><br />
+    <asp:Panel runat="server" ID="status"></asp:Panel><br />
+    <h3>Description</h3><br />
+    <asp:Panel runat="server" ID="description"></asp:Panel><br />
+    <h3>Relation</h3>
+    <asp:Panel runat="server" ID="relations"></asp:Panel><br />
+    <hr />
+
+    <h3>Stakeholder</h3><br />
+    <asp:Panel runat="server" ID="stakeholder"></asp:Panel><br />
+
+
+    <h3>Factors</h3><br />
+    <asp:Panel runat="server" ID="factors"></asp:Panel><br />
+
+    <h3>Artefacts</h3><br />
+    <asp:Panel runat="server" ID="artefacts"></asp:Panel><br />
+    
+    <h3>Documents</h3><br />
+    <asp:Panel runat="server" ID="documents"></asp:Panel><br />
+
+    <h3>User</h3><br />
+    <asp:Panel runat="server" ID="users"></asp:Panel><br />
+
+    <hr />
+
+    <h3>Criteria</h3>
+    <asp:HiddenField ID="HiddenFieldCriteria" runat="server" Value="1" ViewStateMode="Enabled"/>
+    <asp:Table ID="criteriaTable" runat="server" Width="50%" Visible="false">  
+        <asp:TableHeaderRow>                
+                <asp:TableCell>Name</asp:TableCell>
+                <asp:TableCell>Description</asp:TableCell>
+        </asp:TableHeaderRow>
+    </asp:Table>
+
+    <asp:LinkButton runat="server" ID="addCriteria" OnClick="addCriteria_Click">add criteria</asp:LinkButton>
+    <hr />
+
+    <h3>Criteria Weight</h3>
+    <asp:Panel runat="server" ID="criteriaWeight"></asp:Panel><br />
+    <asp:LinkButton runat="server" ID="addComment">add comment</asp:LinkButton>
+    <hr />
+
+    <h3>Alternatives</h3>
+    <asp:Label runat="server" ID="lAlternativeName" Enabled="false">Name</asp:Label><asp:label runat="server" ID="lAlternativeDescription" Enabled="false">Description</asp:label><asp:label runat="server" ID="lAlternativeReason" Enabled="false">Reason</asp:label>
+    <asp:Panel runat="server" ID="alternatives"></asp:Panel><br />
+    <asp:LinkButton runat="server" ID="addAlternative" OnClick="addAlternative_Click">add alternative</asp:LinkButton>
+
+    <button>Save</button> <button>Finish Phase</button>
+</asp:Content>
+
---
+++ Client/IssueDetail.aspx.cs
@@ -0,0 +1,191 @@
+﻿using CDDSS_API.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Client
+{
+    public partial class IssueDetail : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                RestClient rc = RestClient.GetInstance(Session.SessionID);
+
+                if (this.User.Identity.IsAuthenticated && rc != null)
+                {
+                    String issueId = Request.QueryString["issueId"];
+
+                    rc.EndPoint = "api/Issue?issueId=" + issueId;
+                    rc.Method = HttpVerb.GET;
+                    var json = rc.MakeRequest();
+                    var issue = JsonConvert.DeserializeObject<IssueModel>(json);
+
+                    Label lTitle = new Label();
+                    lTitle.Text = issue.Title;
+                    issueTitle.Controls.Add(lTitle);
+
+                    Label lTags;
+
+                    foreach (TagModel tTag in issue.Tags)
+                    {
+                        lTags = new Label();
+                        lTags.Text += tTag.Name;
+                        tag.Controls.Add(lTags);
+                    }
+
+                    Label lStatus = new Label();
+                    lStatus.Text = issue.Status;
+                    status.Controls.Add(lStatus);
+
+                    Label lDescription = new Label();
+                    lDescription.Text = issue.Description;
+                    description.Controls.Add(lDescription);
+
+                    rc.EndPoint = "api/Issue?issueId=" + issue.RelatedTo;
+                    rc.Method = HttpVerb.GET;
+                    var jsonRel = rc.MakeRequest();
+                    var issueRel = JsonConvert.DeserializeObject<IssueModel>(json);
+
+                    Label lRelation = new Label();
+
+                    if (issueRel != null)
+                    {
+                        lRelation.Text = issueRel.Title;
+                    }
+                    else
+                    {
+                        lRelation.Text = "No relations to other Issues";
+                    }
+
+                    relations.Controls.Add(lRelation);
+
+                    Label lStakeholder;
+                    if (issue.Stakeholders.Count > 0)
+                    {
+                        foreach (StakeholderModel stake in issue.Stakeholders)
+                        {
+                            lStakeholder = new Label();
+                            lStakeholder.Text = stake.Name;
+                            stakeholder.Controls.Add(lStakeholder);
+                        }
+                    }
+                    else
+                    {
+                        lStakeholder = new Label();
+                        lStakeholder.Text = "No stakeholders";
+                        stakeholder.Controls.Add(lStakeholder);
+                    }
+
+                    Label lFactor;
+                    if (issue.InfluenceFactors.Count > 0)
+                    {
+                        foreach (InfluenceFactorModel factor in issue.InfluenceFactors)
+                        {
+                            lFactor = new Label();
+                            lFactor.Text = factor.Name;
+                            factors.Controls.Add(lFactor);
+                        }
+                    }
+                    else
+                    {
+                        lFactor = new Label();
+                        lFactor.Text = "No incluence factors";
+                        factors.Controls.Add(lFactor);
+                    }
+
+                    Label lArtefacts;
+                    if (issue.Artefacts.Count > 0)
+                    {
+                        foreach (ArtefactModel artefact in issue.Artefacts)
+                        {
+                            lArtefacts = new Label();
+                            lArtefacts.Text = artefact.Name;
+                            artefacts.Controls.Add(lArtefacts);
+                        }
+                    }
+                    else
+                    {
+                        lArtefacts = new Label();
+                        lArtefacts.Text = "No artefacts";
+                        artefacts.Controls.Add(lArtefacts);
+                    }
+
+                    Label lDocuments;
+                    if (issue.Documents.Count > 0)
+                    {
+                        foreach (String doc in issue.Documents)
+                        {
+                            lDocuments = new Label();
+                            lDocuments.Text = doc;
+                            documents.Controls.Add(lDocuments);
+                        }
+                    }
+                    else
+                    {
+                        lDocuments = new Label();
+                        lDocuments.Text = "No documents";
+                        documents.Controls.Add(lDocuments);
+                    }
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.StackTrace);
+            }
+        }
+
+        protected void addAlternative_Click(object sender, EventArgs e)
+        {
+            lAlternativeName.Enabled = true;
+            lAlternativeDescription.Enabled = true;
+            lAlternativeReason.Enabled = true;
+
+            TextBox tName = new TextBox();
+            TextBox tDescrption = new TextBox();
+            TextBox tReason = new TextBox();
+
+            alternatives.Controls.Add(tName);
+            alternatives.Controls.Add(tDescrption);
+            alternatives.Controls.Add(tReason);
+        }
+
+        protected void addCriteria_Click(object sender, EventArgs e)
+        {
+            int rows = Int32.Parse(HiddenFieldCriteria.Value);
+
+            for (int i = 0; i < rows; i++)
+            {
+                TextBox tbName = new TextBox();
+                tbName.ID = "criteriaName" + (i+1);
+
+                TextBox tbDescrption = new TextBox();
+                tbDescrption.ID = "criteriaDescription" + (i+1);
+
+                TableRow row = new TableRow();
+                TableCell cellName = new TableCell();
+                TableCell cellDesc = new TableCell();
+
+                cellName.Controls.Add(tbName);
+                row.Cells.Add(cellName);
+                cellDesc.Controls.Add(tbDescrption);
+                row.Cells.Add(cellDesc);
+
+                row.Cells.Add(cellName);
+                row.Cells.Add(cellDesc);
+
+                criteriaTable.Rows.Add(row);
+
+            }
+            HiddenFieldCriteria.Value = ""+rows++;
+            criteriaTable.Visible = true;
+        }
+    }
+}
