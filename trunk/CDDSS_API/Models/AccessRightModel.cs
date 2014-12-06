6c28fb16996270e622ecc03dd936a085f5bdd1c2
sinisa.zubic@gmx.at <sinisa.zubic@gmx.at@70bb1a0d-f189-3689-9456-296aa23f10ae>
2014-12-06 21:39:41 +0000

    AccessUserList added to IssueModel

---
+++ trunk/CDDSS_API/Models/AccessRightModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace CDDSS_API.Models
+{
+    [DataContract]
+    public class AccessRightModel
+    {
+        [DataMember]
+        public char Right { get; set; }
+        [DataMember]
+        public UserShort User { get; set; }
+
+        public AccessRightModel()
+        {
+
+        }
+
+        public AccessRightModel(UserShort user, char accessRight)
+        {
+            Right = accessRight;
+            User = user;
+        }
+    }
+}
