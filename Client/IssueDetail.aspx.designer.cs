b6ab429173c4f452de58505421ef9bd25c066a81
daniel.kaltenbrunner <daniel.kaltenbrunner@gmail.com>
2014-12-09 12:10:29 +0000

    Register fixed by Sini

--- Client/IssueDetail.aspx.designer.cs
+++ Client/IssueDetail.aspx.designer.cs
@@ -112,6 +112,15 @@ namespace Client {
         protected global::System.Web.UI.WebControls.Panel users;
         
         /// <summary>
+        /// criteriaPanel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel criteriaPanel;
+        
+        /// <summary>
         /// hiddenCriteria control.
         /// </summary>
         /// <remarks>
