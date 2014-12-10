5be4013cec7264b0ca2fcfb09d12b1f02eab6ae2
daniel.kaltenbrunner@gmail.com <daniel.kaltenbrunner@gmail.com@70bb1a0d-f189-3689-9456-296aa23f10ae>
2014-12-10 15:25:23 +0000

    FileUploadDialog für Sini

--- trunk/Client/CreateIssue.aspx.designer.cs
+++ trunk/Client/CreateIssue.aspx.designer.cs
@@ -229,6 +229,24 @@ namespace Client {
         protected global::System.Web.UI.WebControls.LinkButton addRelation;
         
         /// <summary>
+        /// FileUpload1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.FileUpload FileUpload1;
+        
+        /// <summary>
+        /// UploadButton control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button UploadButton;
+        
+        /// <summary>
         /// addDocument control.
         /// </summary>
         /// <remarks>
