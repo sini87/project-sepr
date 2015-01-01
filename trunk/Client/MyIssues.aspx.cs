1a833340b69aa860675ec7c432407b321f777d31
andreas.knogler87@gmail.com <andreas.knogler87@gmail.com@70bb1a0d-f189-3689-9456-296aa23f10ae>
2015-01-01 21:31:40 +0000

    Implementierung von AddReview

--- trunk/Client/MyIssues.aspx.cs
+++ trunk/Client/MyIssues.aspx.cs
@@ -12,6 +12,20 @@ namespace Client
 {
     public partial class MyIssues : Page
     {
+        Table dataTable = new Table();
+
+        TableRow row;
+        TableCell tTitle;
+        Panel pTitle;
+        Panel pTags;
+        Panel pTag;
+        Panel pRating;
+        Panel pStatus, pStatusParent;
+        Panel pDetail;
+        int hyperlinkid = 0;
+        int rowID = 0;
+        List<Panel> reviewAddPanelList = new List<Panel>();
+
         protected void Page_Preload(object sender, EventArgs e)
         {
             if (!this.User.Identity.IsAuthenticated)
@@ -46,24 +60,15 @@ namespace Client
         protected Table generateTable(List<IssueModel> data)
         {
             if (data.Count > 0)
-            {
-                Table dataTable = new Table();
-
-                TableRow row;
-                TableCell tTitle;
-
-                Panel pTitle;
-                Panel pTags;
-                Panel pTag;
-                Panel pRating;
-                Panel pStatus, pStatusParent;
-                Panel pDetail;
+            {             
 
                 foreach (IssueModel element in data)
                 {
                     row = new TableRow();
+                    row.ID = "tablerow" + rowID;
+                    rowID++;
                     row.CssClass = "row table_row";
-
+                    
                     tTitle = new TableCell();
 
                     pTitle = new Panel();
@@ -116,25 +121,96 @@ namespace Client
                     }
                     pStatusParent.Controls.Add(pStatus);
                     tTitle.Controls.Add(pStatusParent);
+
+                    LinkButton addReviewHyperlink = new LinkButton();
+                    addReviewHyperlink.Text = "Add Review";
+                    addReviewHyperlink.ID = "hyperlinkaddReview" + hyperlinkid;
+                    addReviewHyperlink.Click += new EventHandler(OnAddReviews_Click);
+                    tTitle.Controls.Add(addReviewHyperlink);
+                    LinkButton showReviewHyperlink = new LinkButton();
+                    showReviewHyperlink.Text = "Show Review";
+                    showReviewHyperlink.ID = "hyperlinkshowReview" + hyperlinkid; ;
+                    showReviewHyperlink.Click += new EventHandler(OnShowReviews_Click);
+                    tTitle.Controls.Add(showReviewHyperlink);
+
+                    Panel reviewPanel = CreateAddReviewPanel(element);
+                    reviewPanel.Visible = false;
                     
+                    tTitle.Controls.Add(reviewPanel);
                     row.Cells.Add(tTitle);
 
                     string baseUrl = Request.Url.Scheme + "://" + Request.Url.Authority +
                         Request.ApplicationPath.TrimEnd('/') + "/";
-                    row.Attributes["onclick"] = "javascript:location.href='"+baseUrl+"IssueDetail?issueId=" + element.Id + "'";
+                    pTitle.Attributes["onclick"] = "javascript:location.href='"+baseUrl+"IssueDetail?issueId=" + element.Id + "'";
+                    
 
                     dataTable.Rows.Add(row);
 
                     row = new TableRow();
+                    row.ID = "tablerow" + rowID;
+                    rowID++;
                     row.CssClass = "row spacer";
                     tTitle = new TableCell();
                     row.Cells.Add(tTitle);
                     dataTable.Rows.Add(row);
+
+                    dataTable.Rows.Add(row);
+                    hyperlinkid++;
                 }
                 dataTable.Width = Unit.Percentage(100);
                 return dataTable;
             }
             return null;
+        }
+
+        protected Panel CreateAddReviewPanel(IssueModel element)
+        {
+            Panel reviewPanel = new Panel();
+            reviewPanel.ID = "ReviewPanel" + hyperlinkid;
+            reviewAddPanelList.Add(reviewPanel);
+            Table reviewTable = new Table();
+            reviewTable.Style.Add("margin", "10px");
+            for (int i = 0; i < 5; i++)
+            {
+                TableRow issueRow = new TableRow();
+                TableCell issueLabelCell = new TableCell();
+                Label issueLabel = new Label();
+                TextBox reviewTextBox = new TextBox();
+                issueRow.Style.Add("margin-top", "10px");
+                issueLabelCell.Style.Add("padding", "10px");
+                switch (i)
+                {
+                    case 0: issueLabel.Text = "IssueID"; reviewTextBox.Text = element.Id.ToString();
+                        HiddenField issuehiddenfield = new HiddenField();
+                        issuehiddenfield.Value = element.Id.ToString();
+                        issuehiddenfield.ID = "issueHiddenField" + hyperlinkid;
+                        issueLabelCell.Controls.Add(issuehiddenfield);
+                        reviewTextBox.ID = "issueTextBox" + hyperlinkid; break;
+                    case 1: issueLabel.Text = "Rating";
+                        reviewTextBox.Text = element.ReviewRating.ToString();
+                        reviewTextBox.ID = "ratingTextBox" + hyperlinkid; break;
+                    case 2: issueLabel.Text = "Explanation";
+                        reviewTextBox.Text = element.Description;
+                        reviewTextBox.ID = "explanationTextBox" + hyperlinkid; break;
+                    case 3: issueLabel.Text = "UserFirstName";
+                        reviewTextBox.ID = "userFirstNameTextBox" + hyperlinkid; break;
+                    case 4: issueLabel.Text = "UserLastName";
+                        reviewTextBox.ID = "userLastNameTextBox" + hyperlinkid; break;
+                }
+                issueLabel.Width = 150;
+
+                issueLabelCell.Controls.Add(issueLabel);
+                issueLabelCell.Controls.Add(reviewTextBox);
+                issueRow.Controls.Add(issueLabelCell);
+                reviewTable.Controls.Add(issueRow);
+            }
+            reviewPanel.Controls.Add(reviewTable);
+            Button saveReviewButton = new Button();
+            saveReviewButton.ID = "saveReviewButton" + hyperlinkid;
+            saveReviewButton.Click += new EventHandler(OnSaveReviewButton_Click);
+            saveReviewButton.Text = "Save Review";
+            reviewPanel.Controls.Add(saveReviewButton);
+            return reviewPanel;
         }
 
         protected void Login_Authenticate(object sender, AuthenticateEventArgs e)
@@ -146,5 +222,74 @@ namespace Client
         {
             Response.Redirect("CreateIssue.aspx");
         }
+
+        public void OnShowReviews_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        public void DummyMethod_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void OnSaveReviewButton_Click(object sender, EventArgs e)
+        {
+            Button link = (Button)sender;
+            int pressedbuttonid = getIDNumberOfControl(link.ID);
+            TextBox issueTextBox = (TextBox)reviewAddPanelList[pressedbuttonid].FindControl("issueTextBox" + pressedbuttonid);
+            HiddenField issuehiddenfield = (HiddenField)reviewAddPanelList[pressedbuttonid].FindControl("issueHiddenField" + pressedbuttonid);
+            TextBox ratingTextBox = (TextBox)reviewAddPanelList[pressedbuttonid].FindControl("ratingTextBox" + pressedbuttonid);
+            TextBox explanationTextBox = (TextBox)reviewAddPanelList[pressedbuttonid].FindControl("explanationTextBox" + pressedbuttonid);
+            TextBox userFirstNameTextBox = (TextBox)reviewAddPanelList[pressedbuttonid].FindControl("userFirstNameTextBox" + pressedbuttonid);
+            TextBox userLastNameTextBox = (TextBox)reviewAddPanelList[pressedbuttonid].FindControl("userLastNameTextBox" + pressedbuttonid);
+            ReviewModel newReview = new ReviewModel();
+            newReview.Issue = Convert.ToInt32(issuehiddenfield.Value);
+            newReview.Rating = Convert.ToInt32(ratingTextBox.Text);
+            newReview.Explanation = explanationTextBox.Text;
+            newReview.UserFirstName = userFirstNameTextBox.Text;
+            newReview.UserLastName = userLastNameTextBox.Text;
+        }
+
+        public void OnAddReviews_Click(object sender, EventArgs e)
+        {
+
+            LinkButton link = (LinkButton)sender;
+            int pressedlinkbuttonid = getIDNumberOfControl(link.ID);
+            int i = 0;
+            while (i<=dataTable.Rows.Count-1)
+            {
+                if (dataTable.Rows[i].ID != null && pressedlinkbuttonid == getIDNumberOfControl(dataTable.Rows[i].ID) && pressedlinkbuttonid == getIDNumberOfControl(reviewAddPanelList[i].ID))
+                {
+                    reviewAddPanelList[i].Visible = true;
+                }
+                i++;
+            }
+            
+        }
+
+        protected int getIDNumberOfControl(String tableRowID)
+        {
+            String pressedTableRowNumber=null;
+            for (int i = 0; i <= tableRowID.Length-1; i++)
+            {
+                switch (tableRowID[i])
+                {
+                    case '0': pressedTableRowNumber = pressedTableRowNumber+"0"; break;
+                    case '1': pressedTableRowNumber = pressedTableRowNumber + "1"; break;
+                    case '2': pressedTableRowNumber = pressedTableRowNumber + "2"; break;
+                    case '3': pressedTableRowNumber = pressedTableRowNumber + "3"; break;
+                    case '4': pressedTableRowNumber = pressedTableRowNumber + "4"; break;
+                    case '5': pressedTableRowNumber = pressedTableRowNumber + "5"; break;
+                    case '6': pressedTableRowNumber = pressedTableRowNumber + "6"; break;
+                    case '7': pressedTableRowNumber = pressedTableRowNumber + "7"; break;
+                    case '8': pressedTableRowNumber = pressedTableRowNumber + "8"; break;
+                    case '9': pressedTableRowNumber = pressedTableRowNumber + "9"; break;
+                }
+            }
+            return Convert.ToInt32(pressedTableRowNumber);
+        }
+
+
     }
 }
