624f205385bddb290d73f728ed4d2111b3176148
fj.schmidhammer@gmail.com <fj.schmidhammer@gmail.com@70bb1a0d-f189-3689-9456-296aa23f10ae>
2015-01-27 03:06:53 +0000

    DESIGN -IssueDetail title; rating; tags; status; description; relation

--- trunk/Client/IssueDetail.aspx.designer.cs
+++ trunk/Client/IssueDetail.aspx.designer.cs
@@ -46,7 +46,7 @@ namespace Client {
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label ratingLabel;
+        protected global::System.Web.UI.WebControls.TextBox ratingLabel;
         
         /// <summary>
         /// tagPanel control.
