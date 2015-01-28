af0b0ee193a62775d2c46f2d6f6c0a013f2e416f
daniel.kaltenbrunner <daniel.kaltenbrunner@gmail.com>
2015-01-28 10:24:00 +0000

    UnitTests erweitert

---
+++ UnitTests/Stakeholder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CDDSS_API.Models;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class Stakeholder
+    {
+        [TestMethod]
+        public void getAllStakeholders()
+        {
+            try
+            {
+
+                RestClient.Instance.Login(Credentials.username, Credentials.password);
+                RestClient.Instance.EndPoint = "api/Stakeholders";
+                RestClient.Instance.Method = HttpVerb.GET;
+                var resp = RestClient.Instance.MakeRequest();
+
+                List<StakeholderModel> list = JsonConvert.DeserializeObject<List<StakeholderModel>>(resp);
+
+                Assert.AreNotEqual(resp, "");
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("Exception: " + e.Message);
+            }
+
+        }
+    }
+}
